0f0dfa38567bf7ee3e62fe7f0841df5b7a3d8994
swonbae <sbae13@my.centennialcollege.ca>
2021-07-21 20:01:20 -0400

    Update function based views to class-based API views

--- api/views.py
+++ api/views.py
@@ -1,11 +1,55 @@
+from django.http.response import Http404
 from .models import Article
 from .serializers import ArticleSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import APIView
 
 # Create your views here.
 
+class ArticleList(APIView):
+    
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetails(APIView):
+    
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+'''
 @api_view(['GET', 'POST'])
 def article_list(request):
     # get all articles
@@ -42,3 +86,4 @@ def article_details(request, pk):
     elif request.method == "DELETE":
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
