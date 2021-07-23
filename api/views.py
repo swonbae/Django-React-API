a37ccfbee9afe19dec37f179db897d020a1781a0
swonbae <sbae13@my.centennialcollege.ca>
2021-07-22 20:58:55 -0400

    Updated views using ModelViewSet

--- api/views.py
+++ api/views.py
@@ -1,10 +1,8 @@
 from .models import Article
 from .serializers import ArticleSerializer
 from rest_framework import viewsets
-from rest_framework import mixins
 
 
-class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, 
-                    mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
