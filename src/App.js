8a38b4b87b0203dc5877309023623deae9d7fa31
swonbae <sbae13@my.centennialcollege.ca>
2021-07-26 16:21:03 -0400

    Added React Hooks useContext example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
-import Counter from "./components/Counter";
-import CounterHook from "./components/CounterHook";
-import FetchData from "./components/FetchData";
+import React from "react";
+import ComponentA from "./components/ComponentA";
+// import Counter from "./components/Counter";
+// import CounterHook from "./components/CounterHook";
+// import FetchData from "./components/FetchData";
+
+export const MyContext = React.createContext();
 
 function App() {
   return (
     <div className="container">
       {/* <Counter /> */}
       {/* <CounterHook /> */}
-      <FetchData />
+      {/* <FetchData /> */}
+      <MyContext.Provider value="This is context value">
+        <ComponentA />
+      </MyContext.Provider>
     </div>
   );
 }
---
+++ src/components/ComponentA.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ComponentB from "./ComponentB";
+
+function ComponentA() {
+  return (
+    <div>
+      ComponentA
+      <ComponentB />
+    </div>
+  );
+}
+
+export default ComponentA;
